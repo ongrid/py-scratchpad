e3b6793c8147a8e9315e9e45686ceb8a753f5612
Kirill Varlamov <kirill@ongrid.pro>
2020-11-02 19:29:33 +0300

    Decrease interval, increase numbers

--- async_service.py
+++ async_service.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import random
 
 class Server:
 
@@ -7,10 +8,10 @@ class Server:
 
     async def run(self):
         task_main_loop = asyncio.create_task(self.main_loop())
-        task_sum_loop1 = asyncio.create_task(self.summator_loop(1,100))
-        task_sum_loop2 = asyncio.create_task(self.summator_loop(2,100))
-        task_sum_loop3 = asyncio.create_task(self.summator_loop(3,100))
-        task_sum_loop4 = asyncio.create_task(self.summator_loop(4,100))
+        task_sum_loop1 = asyncio.create_task(self.summator_loop(1,10000))
+        task_sum_loop2 = asyncio.create_task(self.summator_loop(2,10000))
+        task_sum_loop3 = asyncio.create_task(self.summator_loop(3,10000))
+        task_sum_loop4 = asyncio.create_task(self.summator_loop(4,10000))
         await task_main_loop
         await task_sum_loop1
         await task_sum_loop2
@@ -19,15 +20,15 @@ class Server:
 
     async def main_loop(self):
         i = 0
-        for i in range(100):
-            await asyncio.sleep(0.1)
+        for i in range(10000):
+            await asyncio.sleep(random.random()/10000)
             i += 1
             self.counter += 1
             print("main %s" % self.counter)
     
     async def summator_loop(self, name, amount):
         for i in range(amount):
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(random.random()/10000)
             i += 1
             self.counter += 1
             print("summator %s %s" % (name, self.counter))
